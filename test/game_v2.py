3d6766034e6c7a3afe0a1c8daccd88fa7d6cd3ac
HunterV1rus <gerion.morozov@gmail.com>
2022-10-02 16:04:10 +0300

    new commit 16:04

--- test/game_v2.py
+++ test/game_v2.py
@@ -12,8 +12,6 @@ def random_predict(number:int=1) -> int:
             break
     return(count)
 
-#print(f'amount of tries:{random_predict()}')
-
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
 
